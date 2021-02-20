04382e154ef94b61ccd6ad059de74da8264c4c6f
LAPTOP-5K218H50\Supercom - 1 <ritthirach.kongjarit@mail.kmutt.ac.th>
2021-02-21 02:56:23 +0700

    ++ Add the digit system with 'technological barbarian method' ++ Will test

--- Week 3 operators homework/Week 3 operators homework/Program.cs
+++ Week 3 operators homework/Week 3 operators homework/Program.cs
@@ -9,7 +9,7 @@ namespace Week_3_operators_homework
         static void Main(string[] args)
         {
             string affiliate;
-            int password;
+            double password;
             string Input1;
             Console.Write("Please input your password: ");
             Input1 = Console.ReadLine();
@@ -17,6 +17,13 @@ namespace Week_3_operators_homework
             affiliate = Console.ReadLine();
 
             password = int.Parse(Input1);
+            double _6thDigit = password / (Math.Pow(10, 6));
+            double _5thDigit = ((password - (_6thDigit * Math.Pow(10,6))) / Math.Pow(10,5));
+            double _4thDigit = ((password - ((_6thDigit * Math.Pow(10, 6)) + (_5thDigit * Math.Pow(10, 5))  )) / Math.Pow(10, 4));
+            double _3rdDigit = ((password - ((_6thDigit * Math.Pow(10, 6)) + (_5thDigit * Math.Pow(10, 5)) + (_4thDigit * Math.Pow(10, 4)))) / Math.Pow(10, 3));
+            double _2ndDigit = ((password - ((_6thDigit * Math.Pow(10, 6)) + (_5thDigit * Math.Pow(10, 5)) + (_4thDigit * Math.Pow(10, 4)) + (_3rdDigit * Math.Pow(10, 3)))) / Math.Pow(10, 2));
+            double _1stDigit = ((password - ((_6thDigit * Math.Pow(10, 6)) + (_5thDigit * Math.Pow(10, 5)) + (_4thDigit * Math.Pow(10, 4)) + (_3rdDigit * Math.Pow(10, 3)) + (_2ndDigit * Math.Pow(10, 2)))) / Math.Pow(10, 2));
+
 
         }
     }
