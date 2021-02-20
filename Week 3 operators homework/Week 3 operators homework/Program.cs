cbc3923feb4d9cc2154356946f0f7d5cb376c9d2
LAPTOP-5K218H50\Supercom - 1 <ritthirach.kongjarit@mail.kmutt.ac.th>
2021-02-20 17:46:25 +0700

    ++ Create this project in Visual Studio

---
+++ Week 3 operators homework/Week 3 operators homework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Week_3_operators_homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
